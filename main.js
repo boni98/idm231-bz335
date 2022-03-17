f832f4a479e2ff420f87ca071557888ffe33e1b5
boni98 <bonizhang2@gmail.com>
2022-03-17 04:23:36 -0400

    add sounds to zodiac keys

--- main.js
+++ main.js
@@ -70,51 +70,75 @@ function userPicked(zodiacBtn) {
 
     switch (zodiacBtn) {
         case 'aquarius':
+            speakers.src = `sounds/aquarius.mp3`;
+            speakers.play();
             console.log('aquarius was clicked');
             displayObj.src = 'images/aquarius.jpg';
             displayObj.classList.add('visible');
         break;
         case 'pisces':
+            speakers.src = `sounds/pisces.mp3`;
+            speakers.play();
             console.log('pisces was clicked');
             displayObj.src = 'images/pisces.jpg';
         break;
         case 'aries':
+            speakers.src = `sounds/aries.mp3`;
+            speakers.play();
             console.log('aries was clicked');
             displayObj.src = 'images/aries.jpg';
         break;
         case 'taurus':
+            speakers.src = `sounds/taurus.mp3`;
+            speakers.play();
             console.log('taurus was clicked');
             displayObj.src = 'images/taurus.jpg';
         break;
         case 'gemini':
+            speakers.src = `sounds/gemini.mp3`;
+            speakers.play();
             console.log('gemini was clicked');
             displayObj.src = 'images/gemini.jpg';
         break;
         case 'cancer':
+            speakers.src = `sounds/cancer.mp3`;
+            speakers.play();
             console.log('cancer was clicked');
             displayObj.src = 'images/cancer.jpg';
         break;
         case 'leo':
+            speakers.src = `sounds/leo.mp3`;
+            speakers.play();
             console.log('leo was clicked');
             displayObj.src = 'images/leo.jpg';
         break;
         case 'virgo':
+            speakers.src = `sounds/virgo.mp3`;
+            speakers.play();
             console.log('virgo was clicked');
             displayObj.src = 'images/virgo.jpg';
         break;
         case 'libra':
+            speakers.src = `sounds/libra.mp3`;
+            speakers.play();
             console.log('libra was clicked');
             displayObj.src = 'images/libra.jpg';
         break;
         case 'scorpio':
+            speakers.src = `sounds/scorpio.mp3`;
+            speakers.play();
             console.log('scorpio was clicked');
             displayObj.src = 'images/scorpio.jpg';
         break;
         case 'sagittarius':
+            speakers.src = `sounds/sagittarius.mp3`;
+            speakers.play();
             console.log('sagittarius was clicked');
             displayObj.src = 'images/sagittarius.jpg';
         break;
         case 'capricorn':
+            speakers.src = `sounds/capricorn.mp3`;
+            speakers.play();
             console.log('capricorn was clicked');
             displayObj.src = 'images/capricorn.jpg';
         break;

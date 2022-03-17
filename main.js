a97a3df6cbffd498c78701d690c195fed6f4c807
boni98 <bonizhang2@gmail.com>
2022-03-17 16:38:51 -0400

    input zodiac algorithm

--- main.js
+++ main.js
@@ -75,6 +75,9 @@ function userPicked(zodiacBtn) {
             console.log('aquarius was clicked');
             displayObj.src = 'images/aquarius.jpg';
             displayObj.classList.add('visible');
+            zInstrument.innerHTML = "You are a trumpet!";
+            zDescription.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus tortor sit amet sem pellentesque posuere."
+            modal.hidden = !modal.hidden;
         break;
         case 'pisces':
             speakers.src = `sounds/pisces.mp3`;
@@ -146,3 +149,59 @@ function userPicked(zodiacBtn) {
             console.log('A button was clicked!');
     }
 }
+
+    const userSubmitBtn = document.getElementById('submit');
+
+// add Event Listener
+userSubmitBtn.addEventListener('click', function() {
+    //console.log('Submit button clicked!');
+
+    const usersFirstName = document.getElementById('name').value;
+    //console.log('Users name is: ' + usersFirstName);
+
+    const userBday = new Date(document.getElementById('birthday').value);
+    //console.log(userBday);
+
+    const whichMonth = userBday.getUTCMonth() + 1;
+    //console.log(`Users month is: ${whichMonth}`);
+
+    const whichDayOfMonth = userBday.getUTCDate();
+    //console.log(`Users birth day is: ${whichDayOfMonth}`);
+
+    // Zodiac Algorithm
+    if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+        AstroSign = "Cap";
+    } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+        AstroSign = "Sag";
+    } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+        AstroSign = "Sco";
+    } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+        AstroSign = "Lib";
+    } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+        AstroSign = "Vir";
+    } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+        AstroSign = "Leo";
+    } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+        AstroSign = "Can";
+    } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+        AstroSign = "Gem";
+    } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+        AstroSign = "Tau";
+    } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+        AstroSign = "Ari";
+    } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+        AstroSign = "Pis";
+    } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+        AstroSign = "Aqu";
+    }
+    
+    userPicked(AstroSign);
+});
+
+// Modal Functionality
+const modal = document.getElementById('modal');
+const modalCloseBtn = document.getElementById('btn-modal-close');
+modalCloseBtn.addEventListener('click', function() {
+    console.log('modal close clicked!');
+    modal.hidden = !modal.hidden;
+});
